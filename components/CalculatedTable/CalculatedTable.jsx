e09d12c515d7b19c173eb22090a146e29e680d4f
PauloVAlves <contato@pauloalves.me>
2021-03-23 19:47:54 -0300

    Styled the calculated table

--- components/CalculatedTable/CalculatedTable.jsx
+++ components/CalculatedTable/CalculatedTable.jsx
@@ -10,8 +10,10 @@ const CalculatedTable = () => {
         <Portion>Porção: {portion}g</Portion>
         <TableItem className='energy'>
           <ItemName>Valor Energético</ItemName>
-          <ItemQuantity>{calculatedRecipe.kcal.toFixed(1)}kcal</ItemQuantity>
-          <ItemQuantity>{calculatedRecipe.kj.toFixed(1)}kj</ItemQuantity>
+          <div className='calories'>
+            <ItemQuantity>{calculatedRecipe.kcal.toFixed(1)}kcal</ItemQuantity>
+            <ItemQuantity>{calculatedRecipe.kj.toFixed(1)}kj</ItemQuantity>
+          </div>
         </TableItem>
         <TableItem className='props'>
           <ItemName>Carboidratos</ItemName>
@@ -57,34 +59,40 @@ const CalculatedTable = () => {
 };
 
 const StyledTable = styled.div`
-  max-width: 50%;
+  display: block;
+  width: 30%;
   margin: auto;
   margin-bottom: 100px;
+  font-size: 1.5rem;
+  text-align: center;
 `;
-const Portion = styled.div``;
-const ItemName = styled.div``;
-const ItemQuantity = styled.div``;
 
 const TableItem = styled.div`
   display: flex;
+  justify-content: space-between;
   cursor: pointer;
   max-width: 100%;
-  margin: auto;
-  margin-bottom: 10px;
   text-align: left;
+  border-bottom: 1px solid #ccc;
+  padding: 0px 5px;
 
-  h3 {
-    text-align: center;
-  }
-
-  .energy {
-    display: flex;
-    flex-direction: row;
+  &:hover {
+    background-color: #eee;
   }
 
   @media (max-width: 900px) {
     max-width: 90%;
   }
+`;
+
+const Portion = styled.div`
+  margin: auto;
+`;
+const ItemName = styled.p`
+  text-align: left;
+`;
+const ItemQuantity = styled.p`
+  text-align: end;
 `;
 
 export default CalculatedTable;
