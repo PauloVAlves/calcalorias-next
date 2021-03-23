ddf5c7351939d8c512fcf6d6606e72ce0ad37550
PauloVAlves <contato@pauloalves.me>
2021-03-23 15:46:07 -0300

    privacy policy page

---
+++ pages/policy.js
@@ -0,0 +1,215 @@
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+
+function policy() {
+  return (
+    <Privacy>
+      <Head>
+        <title>Política de Privacidade - CalCalorias</title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content='Politica de privacidade'></meta>
+      </Head>
+      <h1>POLÍTICA DE PRIVACIDADE</h1>
+
+      <p>
+        Esta política de privacidade rege o uso dos aplicativos do software
+        CalCalorias para dispositivos móveis que foi criado pela Mad Cat Corp .
+        O aplicativo é uma calculadora de calorias e informações nutricionais.
+      </p>
+
+      <h2>Quais informações o aplicativo obtém e como são usadas?</h2>
+
+      <h3>Informações fornecidas pelo usuário </h3>
+      <p>
+        O Aplicativo obtém as informações fornecidas quando você faz o download
+        e registra o aplicativo. O registro conosco é opcional. No entanto,
+        lembre-se de que você pode não conseguir usar alguns dos recursos
+        oferecidos pelo Aplicativo a menos que se registre conosco.{' '}
+      </p>
+
+      <p>
+        Quando você se registra conosco e usa o Aplicativo, geralmente fornece
+        (a) seu nome, endereço de e-mail, idade, nome de usuário, senha e outras
+        informações de registro; (b) informações relacionadas à transação, como
+        quando você faz compras, responde a quaisquer ofertas ou faz download ou
+        usa nossos aplicativos; (c) informações que você nos fornece quando nos
+        contata para obter ajuda; (d) informações de cartão de crédito para
+        compra e uso do Aplicativo e; (e) informações que você insere em nosso
+        sistema ao usar o Aplicativo, como informações de contato e informações
+        de gerenciamento de projetos. Também podemos usar as informações que
+        você nos forneceu para entrar em contato com você de tempos em tempos
+        para fornecer-lhe informações importantes, avisos necessários e
+        promoções de marketing.
+      </p>
+
+      <h3>Informações coletadas automaticamente </h3>
+
+      <p>
+        Além disso, o Aplicativo pode coletar certas informações
+        automaticamente, incluindo, mas não se limitando a, o tipo de
+        dispositivo móvel que você usa, o ID exclusivo do seu dispositivo móvel,
+        o endereço IP do seu dispositivo móvel, seu sistema operacional móvel, o
+        tipo de móvel Navegadores da Internet que você usa e informações sobre a
+        maneira como você usa o Aplicativo.
+      </p>
+
+      <h3>
+        O aplicativo coleta informações precisas de localização em tempo real do
+        dispositivo?
+      </h3>
+      <p>
+        Este aplicativo não coleta informações precisas sobre a localização do
+        seu dispositivo móvel.
+      </p>
+
+      <h3>
+        Terceiros veem e / ou têm acesso às informações obtidas pelo Aplicativo?
+      </h3>
+      <p>
+        Apenas dados agregados e anônimos são periodicamente transmitidos a
+        serviços externos para nos ajudar a melhorar o Aplicativo e nosso
+        serviço. Compartilharemos suas informações com terceiros apenas nas
+        formas descritas nesta declaração de privacidade.
+      </p>
+      <p>
+        Podemos divulgar informações fornecidas pelo usuário e coletadas
+        automaticamente:
+      </p>
+      <ul>
+        <li>
+          conforme exigido por lei, como para cumprir uma intimação ou processo
+          legal semelhante;
+        </li>
+        <li>
+          quando acreditamos de boa fé que a divulgação é necessária para
+          proteger nossos direitos, proteger sua segurança ou a segurança de
+          outras pessoas, investigar fraude ou responder a uma solicitação
+          governamental;
+        </li>
+        <li>
+          com nossos provedores de serviços confiáveis ​​que trabalham em nosso
+          nome, não têm um uso independente das informações que divulgamos a
+          eles e concordaram em cumprir as regras estabelecidas nesta declaração
+          de privacidade.
+        </li>
+        <li>
+          se a Mad Cat Corp estiver envolvida em uma fusão, aquisição ou venda
+          de todos ou parte de seus ativos, você será notificado por e-mail e /
+          ou um aviso destacado em nosso site sobre qualquer mudança na
+          propriedade ou no uso dessas informações , bem como quaisquer opções
+          que você possa ter em relação a essas informações.
+        </li>
+      </ul>
+
+      <h3>Quais são os meus direitos de opt-out?</h3>
+      <p>
+        Você pode interromper toda a coleta de informações pelo aplicativo
+        facilmente desinstalando o aplicativo. Você pode usar os processos de
+        desinstalação padrão que podem estar disponíveis como parte de seu
+        dispositivo móvel ou através do mercado ou rede de aplicativos móveis.
+        Você também pode solicitar o cancelamento por e-mail, em
+        madcatcorp@gmail.com .
+      </p>
+
+      <h3>Política de retenção de dados, gerenciamento de suas informações</h3>
+      <p>
+        Reteremos os dados fornecidos pelo usuário enquanto você usar o
+        aplicativo e por um período razoável a partir de então. Reteremos as
+        informações coletadas automaticamente por até 24 meses e, a partir daí,
+        podemos armazená-las de forma agregada. Se desejar que excluamos os
+        dados fornecidos pelo usuário fornecidos por meio do Aplicativo, entre
+        em contato conosco pelo e-mail madcatcorp@gmail.com e responderemos em
+        um prazo razoável. Observe que alguns ou todos os Dados fornecidos pelo
+        usuário podem ser necessários para que o CalCalorias funcione
+        corretamente.
+      </p>
+
+      <h3>Crianças</h3>
+      <p>
+        Não usamos o Aplicativo para solicitar intencionalmente dados ou
+        comercializar para crianças menores de 13 anos. Se um pai ou responsável
+        ficar sabendo que seu filho nos forneceu informações sem seu
+        consentimento, ele ou ela deve entrar em contato conosco na
+        madcatcorp@gmail.com . Excluiremos essas informações de nossos arquivos
+        dentro de um prazo razoável.{' '}
+      </p>
+
+      <h3>Segurança</h3>
+      <p>
+        Estamos preocupados em salvaguardar a confidencialidade das suas
+        informações. Oferecemos salvaguardas físicas, eletrônicas e
+        procedimentais para proteger as informações que processamos e mantemos.
+        Por exemplo, limitamos o acesso a essas informações a funcionários
+        autorizados e contratados que precisam saber dessas informações para
+        operar, desenvolver ou melhorar nosso Aplicativo. Esteja ciente de que,
+        embora nos esforçamos para fornecer segurança razoável para as
+        informações que processamos e mantemos, nenhum sistema de segurança pode
+        impedir todas as possíveis violações de segurança.
+      </p>
+
+      <h3>Alterar</h3>
+      <p>
+        Esta Política de Privacidade pode ser atualizada de tempos em tempos por
+        qualquer motivo. Iremos notificá-lo de quaisquer alterações em nossa
+        Política de Privacidade, publicando a nova Política de Privacidade aqui
+        e informando-o por e-mail ou mensagem de texto. Aconselhamos você a
+        consultar esta Política de Privacidade regularmente para quaisquer
+        alterações, já que o uso contínuo é considerado a aprovação de todas as
+        alterações. Você pode verificar o histórico desta política clicando
+        aqui.{' '}
+      </p>
+
+      <h3>Sua permissão</h3>
+      <p>
+        Ao usar o CalCalorias , você concorda com o nosso processamento de suas
+        informações conforme estabelecido nesta Política de Privacidade agora e
+        conforme alterado por nós. "Processamento" significa usar cookies em um
+        computador / dispositivo portátil ou usar ou tocar nas informações de
+        qualquer forma, incluindo, mas não se limitando à coleta, armazenamento,
+        exclusão, uso, combinação e divulgação de informações, todas as quais
+        atividades ocorrerão no Brasil .{' '}
+      </p>
+      <h3>Contate-Nos</h3>
+      <p>
+        Se você tiver alguma dúvida sobre privacidade durante o uso do
+        Calcalorias , ou tiver dúvidas sobre nossas práticas, entre em contato
+        conosco através do e-mail madcatcorp@gmail.com .{' '}
+      </p>
+    </Privacy>
+  );
+}
+
+const Privacy = styled.div`
+  width: 80%;
+  margin: 100px auto;
+
+  h1 {
+    margin-bottom: 40px;
+    text-align: center;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+  h3 {
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    margin-bottom: 40px;
+
+    li {
+      width: 80%;
+      margin-left: 50px;
+    }
+  }
+`;
+
+export default policy;
