86a0306ed497cf740730d7c6612ba306c560787d
PauloVAlves <contato@pauloalves.me>
2021-03-23 17:52:54 -0300

    Form to put food portion and calculate

---
+++ components/form/CalculateForm.jsx
@@ -0,0 +1,193 @@
+import React, { useState, useContext } from 'react';
+import { DataContext } from '../../data/DataContext';
+import Input from './Input';
+import Button from './Button';
+import styled from 'styled-components';
+
+const CalculateForm = () => {
+  const {
+    recipe,
+    setCalculatedRecipe,
+    calculatedRecipe,
+    portion,
+    setPortion,
+    setInitialValue,
+  } = useContext(DataContext);
+
+  const [recipeTotalCalories, setRecipeTotalCalories] = useState(0);
+
+  const base = 100;
+
+  const calculateTotalGrams = (recipe, portion) => {
+    let totalGrams = 0;
+    recipe.map((food) => (totalGrams += food.quantity / portion));
+    return totalGrams;
+  };
+
+  const calculateCarbohydrate = (recipe, totalGrams) => {
+    let totalCarbohydrate = 0;
+    recipe.map((food) => (totalCarbohydrate += food.carbohydrate / totalGrams));
+    return totalCarbohydrate;
+  };
+
+  const calculateProtein = (recipe, totalGrams) => {
+    let totalProtein = 0;
+    recipe.map((food) => (totalProtein += food.protein / totalGrams));
+    return totalProtein;
+  };
+
+  const calculateSaturatedFat = (recipe, totalGrams) => {
+    let totalSaturated = 0;
+    recipe.map((food) => (totalSaturated += food.saturated / totalGrams));
+    return totalSaturated;
+  };
+
+  const calculateTotalFats = (recipe, totalGrams) => {
+    let totalFats = 0;
+    recipe.map(
+      (food) =>
+        (totalFats +=
+          (food.saturated + food.monounsaturated + food.polyunsaturated) /
+          totalGrams)
+    );
+    return totalFats;
+  };
+
+  const calculateCholesterol = (recipe, totalGrams) => {
+    let totalCholesterol = 0;
+    recipe.map((food) => (totalCholesterol += food.cholesterol / totalGrams));
+    return totalCholesterol;
+  };
+
+  const calculateFiber = (recipe, totalGrams) => {
+    let totalFiber = 0;
+    recipe.map((food) => (totalFiber += food.fiber / totalGrams));
+    return totalFiber;
+  };
+
+  const calculateSodium = (recipe, totalGrams) => {
+    let totalSodium = 0;
+    recipe.map((food) => (totalSodium += food.sodium / totalGrams));
+    return totalSodium;
+  };
+
+  const caloriesPortion = (recipe) => {
+    let quantityPortion = 0;
+    let caloriesPortion = 0;
+    recipe.map(
+      (food) => (
+        (quantityPortion = food.quantity / base),
+        (caloriesPortion = food.kcal * quantityPortion)
+      )
+    );
+    setRecipeTotalCalories(caloriesPortion);
+    return caloriesPortion;
+  };
+
+  const calculateCalories = (caloriesPortion, recipe, totalGrams) => {
+    let totalCalories = 0;
+    recipe.map((food) => (totalCalories += caloriesPortion / totalGrams));
+
+    return totalCalories;
+  };
+
+  const calculateJoules = (calories) => {
+    return calories * 4.184;
+  };
+
+  const calculate = (e) => {
+    e.preventDefault();
+    if (recipe.length < 1) {
+      alert('Adicione alimentos à receita');
+    } else if (!portion) {
+      alert('Informe a porção');
+    } else {
+      const totalGrams = calculateTotalGrams(recipe, portion);
+      const calories_portion = caloriesPortion(recipe);
+      const carbohydrate = calculateCarbohydrate(recipe, totalGrams);
+      const protein = calculateProtein(recipe, totalGrams);
+      const saturated = calculateSaturatedFat(recipe, totalGrams);
+      const total_fats = calculateTotalFats(recipe, totalGrams);
+      const cholesterol = calculateCholesterol(recipe, totalGrams);
+      const fiber = calculateFiber(recipe, totalGrams);
+      const sodium = calculateSodium(recipe, totalGrams);
+      const calories = calculateCalories(calories_portion, recipe, totalGrams);
+      const joules = calculateJoules(calories);
+
+      setCalculatedRecipe({
+        kcal: calories,
+        kj: joules,
+        protein: protein,
+        cholesterol: cholesterol,
+        carbohydrate: carbohydrate,
+        fiber: fiber,
+        sodium: sodium,
+        total_fats: total_fats,
+        saturated: saturated,
+        trans: 0,
+        portion: portion,
+      });
+      setInitialValue(1);
+    }
+  };
+  return (
+    <>
+      <StyledForm onSubmit={calculate}>
+        <Input value={portion} onChange={(e) => setPortion(e.target.value)} />
+        <Button buttonName='Calcular' />
+      </StyledForm>
+
+      {calculatedRecipe.kcal >= 0 && (
+        <>
+          <PrintTotalResult>
+            Calorias totais: {recipeTotalCalories.toFixed(1)}
+          </PrintTotalResult>
+          <PrintPortionResult>
+            Calorias por porção: {calculatedRecipe.kcal.toFixed(1)}
+          </PrintPortionResult>
+        </>
+      )}
+    </>
+  );
+};
+
+const StyledForm = styled.form`
+  width: 30%;
+  margin: 30px auto;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+
+  Button {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+  }
+
+  @media (max-width: 900px) {
+    margin: 20px auto;
+    width: 90%;
+
+    Button {
+      width: 80%;
+    }
+  }
+`;
+
+const PrintTotalResult = styled.p`
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: 400;
+  margin-top: 30px;
+`;
+
+const PrintPortionResult = styled.p`
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: 900;
+  margin-top: 30px;
+  margin-bottom: 100px;
+`;
+
+export default CalculateForm;
