47a5ea93686943d35950764ea083781e8e06f685
PauloVAlves <contato@pauloalves.me>
2021-03-24 16:51:05 -0300

    added error and loading components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from './navbar/Navbar';
 import Footer from './footer/Footer';
+import { DataContext } from '../data/DataContext';
+import Loading from './Loading';
+import Error from './Error';
 
 const Layout = ({ children }) => {
-  return (
-    <div>
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
-  );
+  const { isLoaded, error } = useContext(DataContext);
+  if (error) {
+    return (
+      <div>
+        <Navbar />
+        <Error />
+        <Footer />
+      </div>
+    );
+  } else if (!isLoaded) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default Layout;
