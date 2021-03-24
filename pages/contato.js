476d94696f7bf6b38f61ffc58cfa5491168bb933
PauloVAlves <contato@pauloalves.me>
2021-03-24 08:55:58 -0300

    added axios

--- pages/contato.js
+++ pages/contato.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Input from '../components/form/Input';
 import styled from 'styled-components';
 import Head from 'next/head';
